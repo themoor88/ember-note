851cad83c8582a2b9b612d32d78ac07935691178
Wajid Kagzi <wjkagzi@gmail.com>
2015-08-04 21:32:36 -0400

    Added actions in the register route.

--- app/register/route.js
+++ app/register/route.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    addNew: function() {
+      var self = this;
+      var user = this.store.createRecord('user', {
+        name: this.controller.get('name')
+      });
+      user.save().then(function() {
+        console.log("successful");
+        self.controller.set('message', 'A new user with the name "' + self.controller.get('name') + '" was added!');
+        self.controller.set('name', null);
+      }, function() {
+        console.log('save failed');
+      });
+    }
+  }
 });
